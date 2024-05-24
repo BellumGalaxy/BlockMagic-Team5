d34e725e8b875b38afc076a9a7ec59fd15f10154
edlincoln <eduardo.lincoln@gmail.com>
2024-05-24 20:29:10 -0300

    fixing rules

--- src/MyJourney20.sol
+++ src/MyJourney20.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
-import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
+// import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
+// import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
 
 
-contract MyJourney20 is ERC721URIStorage, AccessControl, FunctionsClient {
+contract MyJourney20 is ERC721URIStorage, AccessControl { //}, FunctionsClient {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    using FunctionsRequest for FunctionsRequest.Request;
+    // using FunctionsRequest for FunctionsRequest.Request;
 
     uint256 public s_tokenIdCounter;
 
@@ -24,13 +24,13 @@ contract MyJourney20 is ERC721URIStorage, AccessControl, FunctionsClient {
         uint256 id;
         address studentAddress;
         string name;
-        uint256 dateOfBirth;
     }
 
     mapping(address => Student) public s_students;
+    mapping(address => bool) public s_is_registered;
     mapping(address => EducationStage) public s_studentEducationStage;
     mapping(EducationStage => string) public s_stageNFTLinks;
-    mapping(address => bool) public s_financialInstitutions;
+    mapping(address => bool) public s_educationalInstitutions;
     mapping(address => bool) public s_administrators;
 
     event NFTIssued(address indexed student, EducationStage stage);
@@ -56,7 +56,7 @@ contract MyJourney20 is ERC721URIStorage, AccessControl, FunctionsClient {
     string public s_source = "";
 
 
-    constructor() ERC721("MyJourneyNFT", "MJNFT") FunctionsClient(i_router_add) {
+    constructor() ERC721("MyJourneyNFT", "MJNFT") { // FunctionsClient(i_router_add) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
 
@@ -69,12 +69,16 @@ contract MyJourney20 is ERC721URIStorage, AccessControl, FunctionsClient {
         s_administrators[msg.sender] = true;
     }
 
+    // function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721URIStorage, AccessControl) returns (bool) {
+    //     return ERC721URIStorage.supportsInterface(interfaceId) || AccessControl.supportsInterface(interfaceId) || FunctionsClient.supportsInterface(interfaceId);
+    // }
+
     function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721URIStorage, AccessControl) returns (bool) {
-        return ERC721URIStorage.supportsInterface(interfaceId) || AccessControl.supportsInterface(interfaceId) || FunctionsClient.supportsInterface(interfaceId);
+        return ERC721URIStorage.supportsInterface(interfaceId) || AccessControl.supportsInterface(interfaceId);
     }
 
-    modifier onlyFinancialInstitution() {
-        require(s_financialInstitutions[msg.sender], "Caller is not an authorized financial institution");
+    modifier onlyEducationalInstitution() {
+        require(s_educationalInstitutions[msg.sender], "Caller is not an authorized educational institution");
         _;
     }
 
@@ -83,24 +87,26 @@ contract MyJourney20 is ERC721URIStorage, AccessControl, FunctionsClient {
         _;
     }
 
-    function addStudent(address _studentAddress, uint256 _id, string memory _name, uint256 _dateOfBirth) external onlyFinancialInstitution {
-        s_students[_studentAddress] = Student(_id, _studentAddress, _name, _dateOfBirth);
+    function addStudent(address _studentAddress, uint256 _id, string memory _name) external onlyEducationalInstitution {
+        s_students[_studentAddress] = Student(_id, _studentAddress, _name);
+        s_is_registered[_studentAddress] = true;
     }
 
-    function issueNFT(address _studentAddress) external onlyFinancialInstitution {
-        require(s_students[_studentAddress].studentAddress != address(0), "Student does not exist");
+    function removeStudent(address _studentAddress) external onlyEducationalInstitution {
+        s_is_registered[_studentAddress] = false;
+    }
 
-        EducationStage currentStage = s_studentEducationStage[_studentAddress];
-        
+    function issueNFT(address _studentAddress) external onlyEducationalInstitution {
+        require(s_is_registered[_studentAddress], "Student does not exist");
+        EducationStage currentStage = s_studentEducationStage[_studentAddress];        
         if (balanceOf(_studentAddress) == 0) {
             currentStage = EducationStage.Fundamental;
         } else {
             require(currentStage != EducationStage.University, "Student has completed all stages");
         }
 
-        uint256 tokenId = s_tokenIdCounter.current();
-        s_tokenIdCounter.increment();
-        
+        uint256 tokenId = ++s_tokenIdCounter;
+
         _safeMint(_studentAddress, tokenId);
         _setTokenURI(tokenId, s_stageNFTLinks[currentStage]);
 
@@ -117,15 +123,14 @@ contract MyJourney20 is ERC721URIStorage, AccessControl, FunctionsClient {
         // sendReponse( studentAddress, tokenId);
     }
 
-    function issueNFTForStage(address _studentAddress, EducationStage _stage) external onlyFinancialInstitution {
-        require(s_students[_studentAddress].studentAddress != address(0), "Student does not exist");
+    function issueNFTForStage(address _studentAddress, EducationStage _stage) external onlyEducationalInstitution {
+        require(s_is_registered[_studentAddress], "Student does not exist");
         require(_stage >= EducationStage.Fundamental && _stage <= EducationStage.University, "Invalid education stage");
 
         // Ensure the student doesn't already have an NFT for the given stage or a higher stage
         require(s_studentEducationStage[_studentAddress] < _stage, "Student already has an NFT for this stage or higher");
 
-        uint256 tokenId = s_tokenIdCounter.current();
-        s_tokenIdCounter.increment();
+        uint256 tokenId = ++s_tokenIdCounter;
 
         _safeMint(_studentAddress, tokenId);
         _setTokenURI(tokenId, s_stageNFTLinks[_stage]);
@@ -147,16 +152,16 @@ contract MyJourney20 is ERC721URIStorage, AccessControl, FunctionsClient {
         s_administrators[_adminAddress] = false;
     }
 
-    function addFinancialInstitution(address _institutionAddress) external onlyAdministrator {
-        s_financialInstitutions[_institutionAddress] = true;
+    function addEducationalInstitution(address _institutionAddress) external onlyAdministrator {
+        s_educationalInstitutions[_institutionAddress] = true;
     }
 
-    function removeFinancialInstitution(address _institutionAddress) external onlyAdministrator {
-        s_financialInstitutions[_institutionAddress] = false;
+    function removeEducationalInstitution(address _institutionAddress) external onlyAdministrator {
+        s_educationalInstitutions[_institutionAddress] = false;
     }
     
-    function setNFTMetadataURI(uint256 _tokenId, string memory _tokenURI) external onlyFinancialInstitution {
-        require(_exists(_tokenId), "ERC721URIStorage: URI set of nonexistent token");
+    function setNFTMetadataURI(uint256 _tokenId, string memory _tokenURI) external onlyEducationalInstitution {
+        // require(_exists(_tokenId), "ERC721URIStorage: URI set of nonexistent token");
         _setTokenURI(_tokenId, _tokenURI);
     }
 
@@ -165,33 +170,32 @@ contract MyJourney20 is ERC721URIStorage, AccessControl, FunctionsClient {
         return s_students[_studentAddress];
     }
 
-    function sendReponse(address _studentAddress, uint256 _tokenId) internal {
-        string[] memory args = new string[](1);
+    // function sendReponse(address _studentAddress, uint256 _tokenId) internal {
+    //     string[] memory args = new string[](1);
 
-        Student memory student = getStudentByAddress(_studentAddress);
+    //     Student memory student = getStudentByAddress(_studentAddress);
 
-        FunctionsRequest.Request memory req;
-        req.initializeRequestForInlineJavaScript(s_source); // Initialize the request with JS code
-        req.setArgs(_tokenId);
-        req.setArgs(student.id);
+    //     FunctionsRequest.Request memory req;
+    //     req.initializeRequestForInlineJavaScript(s_source); // Initialize the request with JS code
+    //     req.setArgs(_tokenId);
+    //     req.setArgs(student.id);
 
-        // Send the request and store the request ID
-        s_lastRequestId = _sendRequest(req.encodeCBOR(), s_subscriptionId, GAS_LIMIT, DON_ID);
-    }
+    //     // Send the request and store the request ID
+    //     s_lastRequestId = _sendRequest(req.encodeCBOR(), s_subscriptionId, GAS_LIMIT, DON_ID);
+    // }
 
-        // Receive the weather in the city requested
-    function fulfillRequest(
-        bytes32 requestId,
-        bytes memory response,
-        bytes memory err
-    ) internal override {
-        require(requests[requestId].exists, "request not found");
+    // // Receive the weather in the city requested
+    // function fulfillRequest(
+    //     bytes32 requestId,
+    //     bytes memory response,
+    //     bytes memory err
+    // ) internal override {
+        
+    //     s_lastError = err;
+    //     s_lastResponse = response;
+    //     // timestamp = block.timestamp;
 
-        s_lastError = err;
-        s_lastResponse = response;
-        timestamp = block.timestamp;
-
-        // Emit an event to log the response
-        // emit Response(requestId, lastTemperature, lastResponse, lastError);
-    }
+    //     // Emit an event to log the response
+    //     // emit Response(requestId, lastTemperature, lastResponse, lastError);
+    // }
 }
